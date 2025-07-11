3d47f39d4a5540e0192061d8f800a7da2f45f713
thebilalkhan <bilalkhanusafzai786@gmail.com>
2025-07-11 18:04:53 +0500

    products arrow added

--- resources/views/devlints/index.blade.php
+++ resources/views/devlints/index.blade.php
@@ -264,9 +264,26 @@
 
         <div class="products-container">
 
+{{--            <div class="nav-buttons">--}}
+{{--                <button class="nav-button prev">←</button>--}}
+{{--                <button class="nav-button next">→</button>--}}
+{{--            </div>--}}
+
             <div class="nav-buttons">
-                <button class="nav-button prev">←</button>
-                <button class="nav-button next">→</button>
+                <svg class="nav-button prev" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onclick="prevSlide()">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path d="M5 12H19M5 12L11 6M5 12L11 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </svg>
+                <svg class="nav-button next" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onclick="nextSlide()">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </svg>
             </div>
 
             <div class="slider">
